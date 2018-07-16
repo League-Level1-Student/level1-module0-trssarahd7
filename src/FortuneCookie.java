33a3bcd3e4a9f56618d7ccee4e159cae7c1f6558
league <league@wts5.attlocal.net>
2018-07-15 17:25:22 -0700

    Fortune cookie done, houses not

--- src/FortuneCookie.java
+++ src/FortuneCookie.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
 public class FortuneCookie implements ActionListener {
@@ -18,19 +19,35 @@ public class FortuneCookie implements ActionListener {
 		frame.setVisible(true);
 		JButton button = new JButton();
 		frame.add(button);
-		frame.pack();
 		button.addActionListener(this);
-		int rand = new Random().nextInt(4);
+		JLabel label = new JLabel("Click for a fortune");
+		button.add(label);
+		frame.pack();
 
-		/// 7. Depending on which random number, show one of the five fortunes.
-		// add fortune cookie title
-		// make it clearer why they be clickin
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		System.out.println("Button clicked");
-	if (rand == 0) {
+		int rand = new Random().nextInt(6);
+		if (rand == 0) {
+			JOptionPane.showMessageDialog(null, "You will meet a tortise this week.");
+		}
+		if (rand == 1) {
+			JOptionPane.showMessageDialog(null, "You will drink some really strong coffee next month.");
+		}
+		if (rand == 2) {
+			JOptionPane.showMessageDialog(null, "Oprah Winfrey will adopt you.");
+		}
+		if (rand == 3) {
+			JOptionPane.showMessageDialog(null, "You will forget how to walk tomorrow.");
+		}
+		if (rand == 4) {
+			JOptionPane.showMessageDialog(null, "Your face is now a pie.");
+		}
+		if (rand == 5) {
+			JOptionPane.showMessageDialog(null, "Three ghosts will follow you around for the day.");
+		}
 	}
 }
